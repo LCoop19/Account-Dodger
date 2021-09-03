d3fa49332250700359c08b6394e544ab08b0c4c9
Carlos, Cepeda <carlosalt5126@hotmail.es>
2021-09-03 09:53:02 -0500

    Getting the active tab The function currently logs the url the tab contains, later this is going to check if the tab is currently showing a quora website, maybe.

--- main_code.js
+++ main_code.js
@@ -1,2 +1,18 @@
 //quora
 console.log('this message is to be seen on the console');
+
+let tab= getCurrentTab(); //TODO: do something with 'tab'
+//if the current tab is 'Quora' (maybe checking its url) 
+//then do the action to skip the login prompt.
+
+async function getCurrentTab() {
+    //this code is a sample code taken from:
+    //https://developer.chrome.com/docs/extensions/reference/tabs/#get-the-current-tab
+    //it works.
+
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    console.log(tab.url); //Checking if we retrieved the tab by simply logging the url
+    //into the developer console.
+    return tab;
+  }
